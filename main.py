da3fc1c0fbd7746325f46c7ba5319659f5bc49a6
josefpomikalek <josef_pomikalek@yahoo.com>
2024-10-15 22:54:48 +0200

    Create and config basic window.

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+This app calculates your likely marathon performance based on your personal best over a shorter course.
+"""
+
+from tkinter import *
+
+window = Tk()
+window.title("Marathon Performance Predictor")
+window.config(padx=20, pady=20, height=300, width=600, background="moccasin")
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
