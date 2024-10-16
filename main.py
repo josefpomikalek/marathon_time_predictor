7c5d2b4d7af3071de1e8f3772a712f4546dc5ed0
josefpomikalek <josef_pomikalek@yahoo.com>
2024-10-16 22:31:02 +0200

    Add 2 buttons.

--- main.py
+++ main.py
@@ -15,15 +15,22 @@ personal_best_10km_label.grid(row=0, column=0)
 
 # 10 km PB Entry
 personal_best_10km_entry = Entry(width=20)
-personal_best_10km_entry.grid(row=1, column=0)
+personal_best_10km_entry.grid(row=0, column=1)
 
 # Halfmarathon PB Label
 personal_best_halfmarathon_label = Label(text="What is your personal best for halfmarathon?", background="moccasin")
-personal_best_halfmarathon_label.grid(row=2, column=0)
+personal_best_halfmarathon_label.grid(row=1, column=0)
 
 # Halfmarathon PB Entry
 personal_best_halfmarathon_entry = Entry(width=20)
-personal_best_halfmarathon_entry.grid(row=3, column=0)
+personal_best_halfmarathon_entry.grid(row=1, column=1)
+
+# Buttons
+calculate_from_10km_button = Button(text="Calculate max marathon time")
+calculate_from_10km_button.grid(row=0, column=2)
+
+calculate_from_halfmarathon_button = Button(text="Calculate max marathon time")
+calculate_from_halfmarathon_button.grid(row=1, column=2)
 
 
 
