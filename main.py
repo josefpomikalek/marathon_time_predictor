049f8b76da3fb25aeaa0e23af40b822991ecb05d
josefpomikalek <josef_pomikalek@yahoo.com>
2024-10-16 22:08:18 +0200

    Add 2 labels and 2 entries for 10km's PB and halfmarathon's PB.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-This app calculates your likely marathon performance based on your personal best over a shorter course.
+This app calculates your likely maximal marathon performance based on your personal best over a shorter course.
 """
 
 from tkinter import *
@@ -9,7 +9,21 @@ window = Tk()
 window.title("Marathon Performance Predictor")
 window.config(padx=20, pady=20, height=300, width=600, background="moccasin")
 
+# 10 km PB Label
+personal_best_10km_label = Label(text="What is you personal best for 10 km run?", background="moccasin")
+personal_best_10km_label.grid(row=0, column=0)
 
+# 10 km PB Entry
+personal_best_10km_entry = Entry(width=20)
+personal_best_10km_entry.grid(row=1, column=0)
+
+# Halfmarathon PB Label
+personal_best_halfmarathon_label = Label(text="What is your personal best for halfmarathon?", background="moccasin")
+personal_best_halfmarathon_label.grid(row=2, column=0)
+
+# Halfmarathon PB Entry
+personal_best_halfmarathon_entry = Entry(width=20)
+personal_best_halfmarathon_entry.grid(row=3, column=0)
 
 
 
