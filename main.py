72861f12e8141c9cd7e3e1e428fa71838a2ff650
josefpomikalek <josef_pomikalek@yahoo.com>
2024-10-18 23:49:33 +0200

    Design function to count the marathon time based on 10km PB.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ This app calculates your likely maximal marathon performance based on your perso
 
 from tkinter import *
 from ratios import RATIO_10KM, RATIO_HALF_M
+from math import ceil
 
 window = Tk()
 window.title("Marathon Performance Predictor")
@@ -17,9 +18,30 @@ personal_best_10km_label.grid(row=0, column=0, ipadx=10)
 # 10 km PB Entry
 personal_best_10km_entry = Entry(width=20)
 personal_best_10km_entry.grid(row=0, column=1, padx=20)
+personal_best_10km_entry.focus()
+
+
+def pb_10km():
+
+    minutes = int(personal_best_10km_entry.get()[:2])
+    seconds = int(personal_best_10km_entry.get()[3:])
+    time_10km_in_seconds = minutes * 60 + seconds
+    marathon_in_seconds = ceil(time_10km_in_seconds * RATIO_10KM)
+    print(marathon_in_seconds)
+    hours = marathon_in_seconds // 3600
+    sec = marathon_in_seconds - (hours * 3600)
+    mins = sec //60
+    sec3 = sec - (mins * 60)
+    print(hours)
+    print(sec)
+    print(mins)
+    print(sec3)
+    time = f"{hours}:{mins}:{sec3}"
+    print(time)
+
 
 # 10 km PB Button
-calculate_from_10km_button = Button(text="Calculate max marathon time")
+calculate_from_10km_button = Button(text="Calculate max marathon time", command=pb_10km)
 calculate_from_10km_button.grid(row=0, column=2, ipadx=8, ipady=2, padx=20, pady=10)
 
 # Halfmarathon PB Label
